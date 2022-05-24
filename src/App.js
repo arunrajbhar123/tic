68bf7853c892a49104e5e14caf215d0ae7484f4b
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-05-24 15:55:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TacTicToc from './components/TacTicToc.jsx'
+
+function App() {
+  return (
+    <div className="App">
+     <TacTicToc/>
+    </div>
+  );
+}
+
+export default App;
